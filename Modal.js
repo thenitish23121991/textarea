3cded34d34d06fd92e4151148c9d9e9e9eeaf8bb
unknown <nitsh7084@gmail.com>
2014-01-15 18:04:25 +0530

    Changed mouseup functionality

--- Modal.js
+++ Modal.js
@@ -2,11 +2,13 @@
 
 function Modal(text){
 this.text = text;
+sel_text = text;
 this.buttons = $('.button');
 this.checked = 'none';
 this.overlay = $('.overlay');
 this.modal = $('.modal');
 this.textarea = $('.text');
+this.in_process = false;
 this.create();
 }
 
@@ -29,36 +31,54 @@ var self = this;
 this.overlay.bind('click',function(){
 self.close();
 });
-$.each(this.buttons,function(key,value){
-self.buttons.eq(key).bind('click',function(){
-self.apply_effect(key);
-});
+$('.button').one('click',function(){
+if(self.in_process == false){
+var tex = $(this).text();
+self.apply_effect(tex);
+}
 });
 },
 
-apply_effect : function(key){
+apply_effect : function(tex){
+this.in_process = true;
 var self = this;
-var text = self.buttons.eq(key).text();
-var new_text;
-switch(text){
+//console.log('called');
+//var text = self.buttons.eq(key).text();
+var new_text = '';
+switch(tex){
 
 case 'Bold':
-new_text = '<b>'+self.text+'</b>';
+new_text = '<b>'+sel_text+'</b>';
 break
 
 case 'Arial':
-new_text = '<i>'+self.text+'</i>';
+new_text = '<i>'+sel_text+'</i>';
 break;
 
 case 'Close':
-new_text = self.text;
+new_text = sel_text;
 self.close();
 break;
 }
 var tt1 = this.textarea.val();
-var tt2 = tt1.replace(self.text,new_text);
-this.textarea.val(tt2);
+var tt2 = tt1.replace(sel_text,new_text);
+//console.log(tt2);
+var new_tt2 = this.strip_tags(tt2);
+this.textarea.val(new_tt2);
+this.in_process = false;
 this.close();
+},
+
+strip_tags : function(text){
+new_t = text.replace("<i><i>","<i>");
+new_t = new_t.replace("</i></i>","</i>");
+new_t = new_t.replace("<b><b>","<b>");
+new_t = new_t.replace("</b></b>","</b>");
+new_t = new_t.replace("<i><b>","<i>");
+new_t = new_t.replace("</b></i>","</i>");
+new_t = new_t.replace("<b></i>","<b>");
+new_t = new_t.replace("</b></i>","</b>");
+return new_t;
 },
 
 show_overlay : function(){
@@ -83,6 +103,8 @@ this.modal.css({
 'opacity' : '0',
 '-webkit-transform' : 'translateZ(-300px)'
 });
+self_text = '';
+new_text = '';
 }
 
 }
--- text1.js
+++ text1.js
@@ -12,23 +12,29 @@ inside = true;
 
 });
 
+$('.text').bind('mousedown',function(){
+setTimeout(function(){
+inside = true;
+},600);
+});
 
+$(".text").bind('focus',function(e){
+e.stopPropagation();
+});
 
 $('.text').bind('mouseup',function(e){
-console.log(inside);
 if(inside == true){
 e.stopPropagation();
-console.log(inside);
 if(text1.selectionStart != undefined){
-
 var start_pos = text1.selectionStart;
 var end_pos = text1.selectionEnd;
-selected_text = text1.value.substring(start_pos,end_pos);
+var selected_text = text1.value.substring(start_pos,end_pos);
 
 new Modal(selected_text);
-$(".text").unbind('mouseup');
-}
+inside = false;
 
+}
+text1.selectionStart = 0;
 }
 
 });
